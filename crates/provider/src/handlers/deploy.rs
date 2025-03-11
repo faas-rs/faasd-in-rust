76bd04c94c2f86a518188df59625653a7e6f4162
sparkzky <sparkhhhhhhhhhh@outlook.com>
2025-03-11 15:05:19 +0800

    保存更改

--- crates/provider/src/handlers/deploy.rs
+++ crates/provider/src/handlers/deploy.rs
@@ -1,11 +1,27 @@
-use crate::types;
-
-use super::Handler;
+use super::IAmHandler;
+use crate::types::{self, CreateContainerInfo};
+use actix_web::HttpResponse;
+use service::Service;
+use std::sync::Arc;
 
 pub struct DeployHandler {
     pub config: types::function_deployment::FunctionDeployment,
+    service: Arc<Service>,
 }
 
-impl Handler for DeployHandler {
-    fn get_handler(&self, req: actix_web::HttpRequest) -> impl actix_web::Responder {}
+impl IAmHandler for DeployHandler {
+    type Input = CreateContainerInfo;
+    // type Output = String;
+
+    async fn execute(&self, input: Self::Input) -> impl actix_web::Responder {
+        let cid = input.container_id.clone();
+        let image = input.image.clone();
+        let ns = input.ns.clone();
+        let _ = self
+            .service
+            .create_container(&image, &cid, &ns)
+            .await
+            .unwrap();
+        HttpResponse::Ok().json(format!("Container {} created successfully!", cid))
+    }
 }
