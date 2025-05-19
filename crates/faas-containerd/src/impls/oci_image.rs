59483b16b91ac3006c5a436725f33774501f6e5a
sparkzky <sparkhhhhhhhhhh@outlook.com>
2025-05-19 10:40:29 +0800

    fix: move the snapshot logic into snapshot.rs
    Signed-off-by: sparkzky <sparkhhhhhhhhhh@outlook.com>

--- crates/faas-containerd/src/impls/oci_image.rs
+++ crates/faas-containerd/src/impls/oci_image.rs
@@ -1,11 +1,8 @@
-use super::{ContainerdService, error::ContainerdError};
+use super::ContainerdService;
 
 use container_image_dist_ref::ImgRef;
 use containerd_client::{
-    services::v1::{
-        GetImageRequest, ReadContentRequest, TransferOptions, TransferRequest,
-        snapshots::PrepareSnapshotRequest,
-    },
+    services::v1::{GetImageRequest, ReadContentRequest, TransferOptions, TransferRequest},
     to_any,
     tonic::Request,
     types::{
@@ -18,52 +15,7 @@ use oci_spec::image::{Arch, ImageConfiguration, ImageIndex, ImageManifest, Media
 
 use super::spec::DEFAULT_NAMESPACE;
 
-use super::function::ContainerStaticMetadata;
-
 impl ContainerdService {
-    pub async fn prepare_snapshot(
-        &self,
-        container: &ContainerStaticMetadata,
-    ) -> Result<(), ContainerdError> {
-        let parent_snapshot = self
-            .get_parent_snapshot(&container.image, &container.namespace)
-            .await?;
-        self.do_prepare_snapshot(
-            &container.container_id,
-            &container.namespace,
-            parent_snapshot,
-        )
-        .await?;
-
-        Ok(())
-    }
-
-    async fn do_prepare_snapshot(
-        &self,
-        cid: &str,
-        ns: &str,
-        parent_snapshot: String,
-    ) -> Result<(), ContainerdError> {
-        let req = PrepareSnapshotRequest {
-            snapshotter: "overlayfs".to_string(),
-            key: cid.to_string(),
-            parent: parent_snapshot,
-            ..Default::default()
-        };
-        let mut client = self.client.snapshots();
-        let resp = client
-            .prepare(with_namespace!(req, ns))
-            .await
-            .map_err(|e| {
-                log::error!("Failed to prepare snapshot: {}", e);
-                ContainerdError::CreateSnapshotError(e.to_string())
-            })?;
-
-        log::trace!("Prepare snapshot response: {:?}", resp);
-
-        Ok(())
-    }
-
     async fn get_image(&self, image_name: &str, ns: &str) -> Result<(), ImageError> {
         let mut c = self.client.images();
         let req = GetImageRequest {
@@ -322,44 +274,6 @@ impl ContainerdService {
 
         serde_json::from_slice(&resp)
             .map_err(|e| ImageError::DeserializationFailed(format!("Failed to parse JSON: {}", e)))
-    }
-
-    async fn get_parent_snapshot(
-        &self,
-        image_name: &str,
-        namespace: &str,
-    ) -> Result<String, ContainerdError> {
-        use sha2::Digest;
-        let config = self
-            .image_config(image_name, namespace)
-            .await
-            .map_err(|e| {
-                log::error!("Failed to get image config: {}", e);
-                ContainerdError::GetParentSnapshotError(e.to_string())
-            })?;
-
-        if config.rootfs().diff_ids().is_empty() {
-            log::error!("Image config has no diff_ids for image: {}", image_name);
-            return Err(ContainerdError::GetParentSnapshotError(
-                "No diff_ids found in image config".to_string(),
-            ));
-        }
-
-        let mut iter = config.rootfs().diff_ids().iter();
-        let mut ret = iter
-            .next()
-            .map_or_else(String::new, |layer_digest| layer_digest.clone());
-
-        for layer_digest in iter {
-            let mut hasher = sha2::Sha256::new();
-            hasher.update(ret.as_bytes());
-            ret.push_str(&format!(",{}", layer_digest));
-            hasher.update(" ");
-            hasher.update(layer_digest);
-            let digest = ::hex::encode(hasher.finalize());
-            ret = format!("sha256:{digest}");
-        }
-        Ok(ret)
     }
 }
 
--- crates/faas-containerd/src/impls/snapshot.rs
+++ crates/faas-containerd/src/impls/snapshot.rs
@@ -1,9 +1,13 @@
-use containerd_client::{services::v1::snapshots::MountsRequest, types::Mount, with_namespace};
+use containerd_client::{
+    services::v1::snapshots::{MountsRequest, PrepareSnapshotRequest},
+    types::Mount,
+    with_namespace,
+};
 use tonic::Request;
 
 use crate::impls::error::ContainerdError;
 
-use super::ContainerdService;
+use super::{ContainerdService, function::ContainerStaticMetadata};
 
 impl ContainerdService {
     pub(super) async fn get_mounts(
@@ -27,5 +31,86 @@ impl ContainerdService {
             .mounts;
 
         Ok(mounts)
+    }
+
+    pub async fn prepare_snapshot(
+        &self,
+        container: &ContainerStaticMetadata,
+    ) -> Result<(), ContainerdError> {
+        let parent_snapshot = self
+            .get_parent_snapshot(&container.image, &container.namespace)
+            .await?;
+        self.do_prepare_snapshot(
+            &container.container_id,
+            &container.namespace,
+            parent_snapshot,
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn do_prepare_snapshot(
+        &self,
+        cid: &str,
+        ns: &str,
+        parent_snapshot: String,
+    ) -> Result<(), ContainerdError> {
+        let req = PrepareSnapshotRequest {
+            snapshotter: "overlayfs".to_string(),
+            key: cid.to_string(),
+            parent: parent_snapshot,
+            ..Default::default()
+        };
+        let mut client = self.client.snapshots();
+        let resp = client
+            .prepare(with_namespace!(req, ns))
+            .await
+            .map_err(|e| {
+                log::error!("Failed to prepare snapshot: {}", e);
+                ContainerdError::CreateSnapshotError(e.to_string())
+            })?;
+
+        log::trace!("Prepare snapshot response: {:?}", resp);
+
+        Ok(())
+    }
+
+    async fn get_parent_snapshot(
+        &self,
+        image_name: &str,
+        namespace: &str,
+    ) -> Result<String, ContainerdError> {
+        use sha2::Digest;
+        let config = self
+            .image_config(image_name, namespace)
+            .await
+            .map_err(|e| {
+                log::error!("Failed to get image config: {}", e);
+                ContainerdError::GetParentSnapshotError(e.to_string())
+            })?;
+
+        if config.rootfs().diff_ids().is_empty() {
+            log::error!("Image config has no diff_ids for image: {}", image_name);
+            return Err(ContainerdError::GetParentSnapshotError(
+                "No diff_ids found in image config".to_string(),
+            ));
+        }
+
+        let mut iter = config.rootfs().diff_ids().iter();
+        let mut ret = iter
+            .next()
+            .map_or_else(String::new, |layer_digest| layer_digest.clone());
+
+        for layer_digest in iter {
+            let mut hasher = sha2::Sha256::new();
+            hasher.update(ret.as_bytes());
+            ret.push_str(&format!(",{}", layer_digest));
+            hasher.update(" ");
+            hasher.update(layer_digest);
+            let digest = ::hex::encode(hasher.finalize());
+            ret = format!("sha256:{digest}");
+        }
+        Ok(ret)
     }
 }
